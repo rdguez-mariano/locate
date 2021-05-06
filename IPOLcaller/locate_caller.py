4c09fa3754c7ff300aa313d4e50a606c07b4b340
Mariano Rodriguez <rdguez.mariano@gmail.com>
2021-05-06 11:21:24 +0200

    for IPOL galery

--- IPOLcaller/locate_caller.py
+++ IPOLcaller/locate_caller.py
@@ -18,14 +18,13 @@ elif opt.detector == 'SIFT':
         allmatches, good_HC, KPlist1, KPlist2, H_matrix, ET_KP, ET_M = SIFT_AffNet_HardNet(img1,img2,AffNetBeforeDesc=False,affmaps_method=opt.affmaps, EuPrecision=opt.precision, ransac_iters=opt.ransac_iters)
 
 img3 = cv2.drawMatches(img1,KPlist1,img2,KPlist2,allmatches, None,flags=2)
-warped = 0.3*img2
+# warped = 0.3*img2
 if len(good_HC)>0:
     print("Estimated Homography matrix :\n",H_matrix)
     print("%i matches out of %i are in consensus with the above homography" % (len(good_HC),len(allmatches)))
     h, w = img2.shape[:2]
-    warped = 0.7*cv2.warpPerspective(img1, H_matrix,(w, h)) + warped
-else:
-    print("No consensual matches have been found !")
+    # warped = 0.7*cv2.warpPerspective(img1, H_matrix,(w, h)) + warped
+    warped = cv2.warpPerspective(img1, H_matrix,(w, h))
 
 
 cv2.imwrite(opt.workdir+'all_matches.png',img3)
